0ccc6bc17d2b9a158156ca01c9ef9a80d7d3d965
calebmortensen <caleb.mortensen@live.com>
2024-08-27 11:23:14 -0700

    Initial commit

---
+++ src/main/java/appiumgroupid/Appium/CoreJavaBrushUp1.java
@@ -0,0 +1,17 @@
+package appiumgroupid.Appium;
+
+public class CoreJavaBrushUp1 {
+
+	public static void main(String[] args) {
+		
+		int myNum = 5;
+		String website = "Caleb Mortensen";
+		char letter = 'c';
+		double dec = 5.99;
+		boolean myCard = true;
+		
+		System.out.println("My Variables are: " + myNum + " " + website + " " + letter + " " + dec + " " + myCard);
+
+	}
+
+}
