757eb49676f42e743a4b59aaa5545cb1eea4feb6
calebmortensen <caleb.mortensen@live.com>
2024-08-28 14:25:51 -0700

    Appium Inspector
    Installed Appium Inspector and set paths to app, deviceName, platformName, automationName Used locator to test access to app.

--- src/main/java/setup/AppiumBasics.java
+++ src/main/java/setup/AppiumBasics.java
@@ -1,11 +1,14 @@
 package setup;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
@@ -31,9 +34,12 @@ public class AppiumBasics {
 		AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
 		
 		//Actual Automation
+		//Locators: xpath, id, accessibilityId, classname, androidAutomator - use Appium Inspector
+		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
 		
-		driver.quit();
-		service.stop();
+		
+		//driver.quit();
+		//service.stop();
 		
 	}
 }
