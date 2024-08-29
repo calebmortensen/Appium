072d40d55be747fbc189530764999f9fb1effb3f
calebmortensen <caleb.mortensen@live.com>
2024-08-28 18:54:24 -0700

    BaseClass Creation
    Setup BaseClass with Before and After Annotations to start Appium

--- src/main/java/setup/AppiumBasics.java
+++ src/main/java/setup/AppiumBasics.java
@@ -1,45 +1,21 @@
 package setup;
 
-import org.openqa.selenium.By;
+
 import org.testng.annotations.Test;
-
-import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URISyntaxException;
-
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
 
-public class AppiumBasics {
+
+public class AppiumBasics extends BaseTest {
 
 	@Test
-	public void AppiumTest() throws MalformedURLException, URISyntaxException
+	public void WifiSettingsName() throws MalformedURLException, URISyntaxException
 	{
-		//code to start automation instead of running appium from command line
-		AppiumDriverLocalService service = new AppiumServiceBuilder().withAppiumJS(new File("C:\\Users\\caleb\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
-				.withIPAddress("127.0.0.1").usingPort(4723).build();
-		service.start();
 		
-		//Appium code -> Appium Server -> Mobile
-		//capabilities is android or ios etc
-		
-		UiAutomator2Options options = new UiAutomator2Options();
-		options.setDeviceName("MP35"); //emulator
-		options.setApp("C:\\Users\\caleb\\EWS\\Appium\\src\\main\\java\\resources\\ApiDemos-debug.apk");
-		
-		AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
-		
-		//Actual Automation
-		//Locators: xpath, id, accessibilityId, classname, androidAutomator - use Appium Inspector
 		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
 		
 		
-		//driver.quit();
-		//service.stop();
 		
 	}
 }
---
+++ src/main/java/setup/BaseTest.java
@@ -0,0 +1,51 @@
+package setup;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+
+public class BaseTest {
+	public AndroidDriver driver;
+	public AppiumDriverLocalService service;
+	
+	@BeforeClass
+	public void ConfigureAppium() throws MalformedURLException, URISyntaxException{
+		
+		//code to start automation instead of running appium from command line
+				service = new AppiumServiceBuilder().withAppiumJS(new File("C:\\Users\\caleb\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
+						.withIPAddress("127.0.0.1").usingPort(4723).build();
+				service.start();
+				
+				//Appium code -> Appium Server -> Mobile
+				//capabilities is android or ios etc
+				
+				UiAutomator2Options options = new UiAutomator2Options();
+				options.setDeviceName("MP35"); //emulator
+				options.setApp("C:\\Users\\caleb\\EWS\\Appium\\src\\main\\java\\resources\\ApiDemos-debug.apk");
+				
+				driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+				
+												
+			}
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+		service.stop();
+		
+		
+	}
+	
+	
+	
+}
