82235636c27ad3ee48708f3edc0d2368069f38be
calebmortensen <caleb.mortensen@live.com>
2024-09-02 10:59:58 -0700

    Mobile Browser Test
    Updated version of ChromeDriver, used SelectorsHub to access values

---
+++ src/main/java/mobileBrowser/MobileBrowserTest.java
@@ -0,0 +1,28 @@
+package mobileBrowser;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MobileBrowserTest extends setup.BrowserBaseTest{
+
+	@Test
+	public void browserTest() throws InterruptedException {
+		
+		driver.get("https://rahulshettyacademy.com/angularAppdemo");
+		//Click Hamburger menu
+		driver.findElement(By.xpath("//span[@class='navbar-toggler-icon']")).click();
+		//Click Products
+		driver.findElement(By.cssSelector("a[routerlink*='/products']")).click();
+		//Scroll Down
+		((JavascriptExecutor)driver).executeScript("window.scrollBy(0,1000)","");
+		//Click Devops link
+		String text = driver.findElement(By.cssSelector("a[href*='products/3']")).getText();
+		Assert.assertEquals(text, "Devops");
+		
+	}
+	
+	
+}
